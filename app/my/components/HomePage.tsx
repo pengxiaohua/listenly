451592a0b4e7e9c97cccc08b1c82df758373652a
pengxiaohua <609370075@qq.com>
2026-03-11 00:34:00 +0800

    feat: 删除无用代码

--- app/my/components/HomePage.tsx
+++ app/my/components/HomePage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuthStore } from '@/store/auth'
 import StudyHeatmap from "./StudyHeatmap"
 import LearningActivityPeriod from "./LearningActivityPeriod"
